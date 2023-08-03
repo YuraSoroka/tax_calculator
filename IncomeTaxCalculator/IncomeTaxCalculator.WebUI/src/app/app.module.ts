138129c211ece8182d20e824479a0872e36a23b7
Yurii Soroka <yurasoroka1999@gmail.com>
2023-08-03 17:12:40 +0300

    feat: added base markup for tax calculator

--- IncomeTaxCalculator/IncomeTaxCalculator.WebUI/src/app/app.module.ts
+++ IncomeTaxCalculator/IncomeTaxCalculator.WebUI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaxCalculationComponent } from './pages/tax-calculation/tax-calculation/tax-calculation.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaxCalculationComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
