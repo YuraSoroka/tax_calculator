422436c8401d2d3eb5d7201d1362cf0728832a38
Yurii Soroka <yurasoroka1999@gmail.com>
2023-08-04 20:54:26 +0300

    feat: UI input validation

--- IncomeTaxCalculator/IncomeTaxCalculator.WebUI/src/app/pages/tax-calculation/tax-calculation/tax-calculation.component.ts
+++ IncomeTaxCalculator/IncomeTaxCalculator.WebUI/src/app/pages/tax-calculation/tax-calculation/tax-calculation.component.ts
@@ -1,6 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { TaxCalculationService } from 'src/app/core/services/tax-calculation.service';
 import { TaxResult } from 'src/app/shared/models/tax-result.model';
 
@@ -13,19 +14,36 @@ export class TaxCalculationComponent implements OnInit {
 
   public result: Observable<TaxResult>;
   public grossAnnualSalaryForm: FormGroup
+  public errorMsg: string | null;
 
   constructor(private taxCalculationService: TaxCalculationService,
               private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.grossAnnualSalaryForm = this.formBuilder.group({
-      annualGrossSalary: ["", Validators.required]
+      annualGrossSalary: [""]
     });
   }
 
   calculateTax(){
+    this.errorMsg = null;
+    this.result = this.taxCalculationService.calculateIncomeTax(this.grossAnnualSalaryForm.get("annualGrossSalary")?.value)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.errorMsg = error.error.errors["GrossAnnualSalary"];
+        return throwError(() => error)
+      })
+    );
+  }
 
-    this.result = this.taxCalculationService.calculateIncomeTax(this.grossAnnualSalaryForm.get("annualGrossSalary")?.value);
+  filterInput(event: any){
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode !== 45 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+      this.errorMsg = "Please, enter only numeric value";
+      return false;
+    }
+    this.errorMsg = null;
+    return true;
   }
 
 }
