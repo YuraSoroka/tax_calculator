f91ce554eff9982041d0561e981b087a0832b8e8
Yurii Soroka <yurasoroka1999@gmail.com>
2023-08-06 15:50:37 +0300

    feat: added unit test project for "Application" project

---
+++ IncomeTaxCalculator/Application.UnitTests/Commands/Tax/CalculateTaxCommandTest.cs
@@ -0,0 +1,38 @@
+﻿using IncomeTaxCalculator.Application.Commands.Tax;
+using IncomeTaxCalculator.Application.Common.Interfaces;
+using Moq;
+using FluentAssertions;
+using IncomeTaxCalculator.Domain.Entities;
+
+namespace Application.UnitTests.Commands.Tax
+{
+    public class CalculateTaxCommandTest
+    {
+        private readonly Mock<IAnnualTaxCalculator> _annualTaxCalculatorMock;
+        private readonly Mock<ITaxHistoryWriter> _taxHistoryWriter;
+
+        public CalculateTaxCommandTest()
+        {
+            _annualTaxCalculatorMock = new();
+            _taxHistoryWriter = new();
+        }
+
+        [Fact]
+        public async Task CalculateTaxCommandHandler_ExecutesWithSalary_ReturnTaxResultObject()
+        {
+            // Arrange
+            var command = new CalculateTaxCommand
+            {
+                GrossAnnualSalary = 40000
+            };
+            var handler = new CalculateTaxCommandHandler(_annualTaxCalculatorMock.Object, _taxHistoryWriter.Object);
+
+            // Act
+            var taxResult = await handler.Handle(command ,default);
+
+            // assert
+            taxResult.Should().NotBeNull();
+            taxResult.Should().BeOfType<TaxResult>();
+        }
+    }
+}
