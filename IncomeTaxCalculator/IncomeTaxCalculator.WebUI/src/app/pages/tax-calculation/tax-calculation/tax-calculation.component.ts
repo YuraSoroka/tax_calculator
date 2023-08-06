70794788c95560ea86d6d24bb5f6bad0692122b3
Yurii Soroka <yurasoroka1999@gmail.com>
2023-08-06 17:27:41 +0300

    feat: added a point as a divider

--- IncomeTaxCalculator/IncomeTaxCalculator.WebUI/src/app/pages/tax-calculation/tax-calculation/tax-calculation.component.ts
+++ IncomeTaxCalculator/IncomeTaxCalculator.WebUI/src/app/pages/tax-calculation/tax-calculation/tax-calculation.component.ts
@@ -38,7 +38,7 @@ export class TaxCalculationComponent implements OnInit {
 
   filterInput(event: any){
     const charCode = (event.which) ? event.which : event.keyCode;
-    if (charCode !== 45 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+    if (charCode !== 45 && charCode !== 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
       this.errorMsg = "Please, enter only numeric value";
       return false;
     }
